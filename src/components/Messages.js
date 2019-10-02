0256bd3c28cddc8403eb98dad4c8b05894a076fe
vivekgondaliya <vivekgondaliyaatwork@gmail.com>
2019-10-02 07:23:21 -0400

    nested routing begins

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Messages = () => (
-  <div>
-    <h2>Messages</h2>
-    Messages
-  </div>
+  <ul>
+    {
+        [...Array(5).keys()].map(n => {
+            return <li key={n}>
+                    <Link to={`messages/${n+1}`}>
+                      Message {n+1}
+                    </Link>
+                  </li>;
+        })
+    }
+  </ul>
 );
 
 export default Messages;

bb9613efa965a88505718356f9b55b51efe6a0fa
vivekgondaliya <vivekgondaliyaatwork@gmail.com>
2019-10-02 07:47:52 -0400

    nested routing begginer complete, app not showing error when url path does not exist

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, Route, Switch} from 'react-router-dom';
 
-const Messages = () => (
-  <ul>
-    {
-        [...Array(5).keys()].map(n => {
-            return <li key={n}>
-                    <Link to={`messages/${n+1}`}>
-                      Message {n+1}
-                    </Link>
-                  </li>;
-        })
-    }
-  </ul>
+import Message from './Message';
+
+const Messages = ({ match }) => (
+  <div>
+    <ul>
+      {
+          [...Array(5).keys()].map(n => {
+              return <li key={n}>
+                      <Link to={`${match.url}/${n+1}`}>
+                        Message {n+1}
+                      </Link>
+                    </li>;
+          })
+      }
+    </ul>
+    <Switch>
+      {/* <Route
+        path={match.url}
+        render={() => 
+          <div>
+            <h3>Please select a message from above list</h3>
+            <p>URL <code><strong>messages/:id</strong></code> does not exist</p>
+          </div>
+        }
+      /> */}
+      <Route path={`${match.url}/:id(\\d+)`} component={Message} />
+    </Switch>
+  </div>
 );
 
 export default Messages;

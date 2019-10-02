d62734502ffecf3e4de4422402196cf91046d291
Vivek Gondalia <vivekgondalia@Viveks-MacBook-Pro.local>
2019-10-02 07:14:34 -0400

    client side - basic routing setup

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Route, Link, Switch, Redirect} from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
+import Messages from './components/Messages';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="menu">
+          <ul>
+            <li> <Link to="/">Home</Link> </li>
+            <li> <Link to="/messages">Messages</Link> </li>
+            <li> <Link to="/about">About</Link> </li>
+          </ul>
+      </div>
+      <div className="App-intro">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/messages" component={Messages} />
+            <Route path="/about" component={About} />
+            <Redirect to="/" />
+          </Switch>
+        </div>
     </div>
   );
 }
